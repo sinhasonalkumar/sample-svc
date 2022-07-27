396b17e3f71f518972dc1e2f75767449eab5a338
Sonal Kumar Sinha <sinha.sonal.kumar@gmail.com>
2022-07-27 14:58:08 -0600

    feat:test commmit

---
+++ src/main/java/com/sonal/springboot/samplesvc/samplesvc/SampleSvcApplication.java
@@ -0,0 +1,13 @@
+package com.sonal.springboot.samplesvc.samplesvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleSvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sonal/springboot/samplesvc/samplesvc/SampleSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sonal.springboot.samplesvc.samplesvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
